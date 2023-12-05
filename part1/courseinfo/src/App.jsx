3f73457429c9b116f84988eceb142d2c70640374
jemcv <rangothechameleon@gmail.com>
2023-12-05 22:54:08 +0800

    Done part1, courseinfo step1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,18 +1,26 @@
 const Header = (props) => {
  return (
-    <h1>{props.course}</h1>
+    <>
+      <h1>{props.course}</h1>
+    </>
  )
 }
 
 const Content = (props) => {
   return (
-    <p>{props.part} {props.exercises}</p>
+    <>
+      <p>{props.part1} {props.exercises1}</p>
+      <p>{props.part2} {props.exercises2}</p>
+      <p>{props.part3} {props.exercises3}</p>
+    </>
   )
 }
 
 const Total = (props) => {
   return (
+    <>
       <p>Number of total exercises is {props.total}</p>
+    </>
   )
 }
 
@@ -27,11 +35,9 @@ const App = () => {
 
   return (
     <>
-    <Header course={course}/>
-    <Content part={part1} exercises={exercises1}/>
-    <Content part={part2} exercises={exercises2}/>
-    <Content part={part3} exercises={exercises3}/>
-    <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course}/>
+      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
     </>
   )
  }
