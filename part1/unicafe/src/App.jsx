707ba02dacc2c9e2405f3311eb58344e6122f0cf
jemcv <rangothechameleon@gmail.com>
2023-12-19 14:31:57 +0800

    Done unicafe, step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,9 +5,27 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const giveGoodFeedback = () => {
+    setGood(good + 1)
+  }
+
+  const giveNeutralFeedback = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const giveBadFeedback = () => {
+    setBad (bad + 1)
+  }
+
   return (
     <div>
-        <h1>Hello World!</h1>
+        <h1>give feedback</h1>
+        <button onClick={() => giveGoodFeedback()}>good</button>
+        <button onClick={() => giveNeutralFeedback()}>neutral</button>
+        <button onClick={() => giveBadFeedback()}>bad</button>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
     </div>
   )
 }
