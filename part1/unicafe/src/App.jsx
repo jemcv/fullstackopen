e6cee5cc7a7c02b1c3a116711f9d281973322f01
jemcv <rangothechameleon@gmail.com>
2023-12-19 16:02:25 +0800

    Done unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,28 +4,56 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const giveGoodFeedback = () => {
-    setGood(good + 1)
+  const getGoodFeedback = () => {
+    setGood(good + 1);
+    const updatedGood = good + bad + neutral + 1;
+    setAll(updatedGood);
+    const positive = (good / updatedGood) * 100;
+    setPositive(positive);
+    const totalScore =  good * 1 + neutral * 0 + bad * -1;
+    const totalAve = totalScore / updatedGood 
+    setAverage(totalAve);
   }
 
-  const giveNeutralFeedback = () => {
-    setNeutral(neutral + 1)
+  const getNeutralFeedback = () => {
+    setNeutral(neutral + 1);
+    const updatedNeutral = good + bad + neutral + 1;
+    setAll(updatedNeutral);
+    const positive = (good / updatedNeutral) * 100;
+    setPositive(positive);
+    const totalScore = good * 1 + neutral * 0 + bad * -1;
+    const totalAve = totalScore / updatedNeutral
+    setAverage(totalAve);
   }
 
-  const giveBadFeedback = () => {
-    setBad (bad + 1)
+  const getBadFeedback = () => {
+    setBad(bad + 1);
+    const updatedBad = good + bad + neutral + 1;
+    setAll(updatedBad);
+    const positive = (good / updatedBad) * 100;
+    setPositive(positive);
+    const totalScore = good * 1 + neutral * 0 + bad * -1;
+    const totalAve = totalScore / updatedBad
+    setAverage(totalAve);
   }
 
   return (
     <div>
         <h1>give feedback</h1>
-        <button onClick={() => giveGoodFeedback()}>good</button>
-        <button onClick={() => giveNeutralFeedback()}>neutral</button>
-        <button onClick={() => giveBadFeedback()}>bad</button>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
+          <button onClick={() => getGoodFeedback()}>good</button>
+          <button onClick={() => getNeutralFeedback()}>neutral</button>
+          <button onClick={() => getBadFeedback()}>bad</button>
+        <h1>statistics</h1>
+          <p>good {good}</p>
+          <p>neutral {neutral}</p>
+          <p>bad {bad}</p>
+          <p>all {all}</p>
+          <p>average {average} </p>
+          <p>positive {positive} %</p>
     </div>
   )
 }
