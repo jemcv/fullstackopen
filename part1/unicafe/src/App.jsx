12ec6c09d79d53de858d5353ee9c5d83c6283a39
jemcv <rangothechameleon@gmail.com>
2023-12-19 16:28:17 +0800

    Update whitespace

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -7,7 +7,7 @@ const Statistics = (props) => {
       <p>good {props.good}</p>
       <p>neutral {props.neutral}</p>
       <p>bad {props.bad}</p>
-      <p>all{props.all}</p>
+      <p>all {props.all}</p>
       <p>average {props.average}</p>
       <p>positive {props.positive} %</p>
     </div>
