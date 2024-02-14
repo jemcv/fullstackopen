81cfc9e2d45050f780fea031c41ec4b9210ac67b
jemcv <rangothechameleon@gmail.com>
2024-02-15 03:24:58 +0800

    Done part2, phonebook step6

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,19 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
+  const [persons, setPersons] = useState([])
+  /*[
     { name: 'Arto Hellas', number: '040-123456', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  ]*/
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filteredPersons, setFilteredPersons] = useState(persons)
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3002/persons').then(response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+      setFilteredPersons(response.data)
+    })
+  }, [])
+
+  console.log(persons.length)
 
   const addName = (event) => {
     event.preventDefault()
@@ -52,11 +64,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <Filter handleSearch={handleSearch}/>
+      <Filter handleSearch={handleSearch} />
       <h2>Add a new</h2>
-      <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange}/>
+      <PersonForm addName={addName} newName={newName} newNumber={newNumber} handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons persons={filteredPersons}/>
+      <Persons persons={filteredPersons} />
     </div >
   )
 }
