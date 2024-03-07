0c0224404ab94dc08f8bd4db320c1e5b188d427c
jemcv <rangothechameleon@gmail.com>
2024-03-08 01:44:41 +0800

    Done part3, phonebook backend step2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -29,7 +29,11 @@ let persons = [
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
-  
+
+app.get('/info', (request, response) => {
+    response.send(`<p>Phonebook has info for ${persons.length} people <br/> <p>${Date()}</p></p>`);
+})
+
 app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
