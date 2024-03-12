b29a959a78de0ef8740f17d3b923fcfb85dd0ff4
jemcv <rangothechameleon@gmail.com>
2024-03-12 22:22:04 +0800

    Done part3, phonebook database step4 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -57,13 +57,17 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
+app.get('/api/persons/:id', (request, response, next) => {
+    Person.findById(request.params.id)
+    .then(person => {
         if (person) {
             response.json(person)
         } else {
             response.status(404).end()
         }
+    })
+    .catch(error => {
+        next(error)
     })
 })
 
@@ -93,12 +97,31 @@ app.post('/api/persons', (request, response) => {
     })
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
     Person.findByIdAndDelete(request.params.id)
     .then(result => {
         response.status(204).end()
-    })    
+    })  
+    .catch(error => {
+        next(error)
+    })
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+  
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } 
+    next(error)
+}
+
+app.use(errorHandler)
   
 const PORT = process.env.PORT
 app.listen(PORT, () => {
