c174d9ad6f2e35e0a9f51583e354e6d783735589
jemcv <rangothechameleon@gmail.com>
2024-03-25 11:18:13 +0800

    Done part4, bloglist expansions step5 4.17

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,38 @@
 const blogRouter = require('express').Router()
-require('express-async-errors');
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
   
 blogRouter.post('/', async (request, response) => {
-  const { title, url } = request.body;
+  const body = request.body
 
-  if (!title || !url) {
+  if (!body.title || !body.url) {
     return response.status(400).json({ error: 'Bad Request' });
   }
-  const blog = new Blog(request.body);
-  const result = await blog.save();
-  response.status(201).json(result);
+
+  const user = await User.findById(body.userId)
+
+  if (!user) {
+    return response.status(400).json({ error: 'Invalid user ID' });
+  }
+
+  const blog = new Blog({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
+  });
+
+  const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
+
+  response.status(201).json(savedBlog);
 });
 
 blogRouter.delete('/:id', async (request, response) => {
--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -1,10 +1,9 @@
 const userRouter = require('express').Router()
 const bcrypt = require('bcryptjs')
-require('express-async-errors')
 const User = require('../models/user')
 
 userRouter.get('/', async (request, response) => {
-    const users = await User.find({});
+    const users = await User.find({}).populate('blogs', { url: 1, title: 1, author: 1, likes: 1});
     response.json(users);
 });
 
@@ -31,11 +30,13 @@ userRouter.post('/', async (request, response, next) => {
 
     const saltRounds = 10;
     const passwordHash = await bcrypt.hash(password, saltRounds);
+
     const user = new User({
       username,
       name,
       passwordHash
     });
+    
     const savedUser = await user.save();
     response.status(201).json(savedUser);
   } catch (error) {
