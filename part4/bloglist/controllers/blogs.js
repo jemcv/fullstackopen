e123bd06d8e8cfa8d9ff4bdde88f384f2280a914
jemcv <rangothechameleon@gmail.com>
2024-03-27 12:26:54 +0800

    Done part4, bloglist expansions step7 4.19

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
 
 blogRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
@@ -14,8 +23,13 @@ blogRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'Bad Request' });
   }
 
-  const user = await User.findById(body.userId)
-
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+  
+  const user = await User.findById(decodedToken.id)
+  
   if (!user) {
     return response.status(400).json({ error: 'Invalid user ID' });
   }
