0c2b2a0db6d17322b85490ebd0960ca0a2cc60c9
jemcv <rangothechameleon@gmail.com>
2024-09-04 15:04:54 +0800

    Done part5 step7 bloglist-frontend

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  };
+
+  const toggleVisibility = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button onClick={toggleVisibility}>
+          {visible ? 'hide' : 'view'}
+        </button>
+      </div>
+      {visible && (
+        <div>
+          <a href={blog.url} target="_blank" rel="noopener noreferrer">{blog.url}</a>
+          <br />
+          likes: 0
+          <br />
+          {blog.author}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Blog;
