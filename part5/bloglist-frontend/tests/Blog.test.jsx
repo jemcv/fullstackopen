a60b8f9c1c75329c1c9050974aafc1ba2cf21bc0
jemcv <rangothechameleon@gmail.com>
2024-09-05 17:00:38 +0800

    Done part5 bloglist-tests step2 5.14

--- part5/bloglist-frontend/tests/Blog.test.jsx
+++ part5/bloglist-frontend/tests/Blog.test.jsx
@@ -1,17 +1,43 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../src/components/Blog'
+import BlogForm from '../src/components/BlogForm'
 
 test('renders blog', () => {
-    const blog = {
-        title: 'Component testing is done with react-testing-library',
-        author: 'Test Author',
-        user: {
-            username: 'testuser'
-        }
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Test Author',
+    user: {
+      username: 'testuser'
     }
+  }
 
-    const  { container } = render(<Blog blog={blog} />)
+  const  { container } = render(<Blog blog={blog} />)
 
-    const div = container.querySelector('.blog')
-    expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+  const div = container.querySelector('.blog')
+  expect(div).toHaveTextContent('Component testing is done with react-testing-library')
+})
+
+test('shows blog details when view button is clicked', async () => {
+  const blog = {
+    title: 'Component testing is done with react-testing-library',
+    author: 'Test Author',
+    url: 'http://testurl.com',
+    likes: 5,
+    user: {
+      username: 'testuser'
+    }
+  }
+
+  const { container } = render(<Blog blog={blog} />)
+  const user = userEvent.setup()
+
+  expect(container).not.toHaveTextContent('http://testurl.com')
+  expect(container).not.toHaveTextContent('likes 5')
+
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  expect(container).toHaveTextContent('http://testurl.com')
+  expect(container).toHaveTextContent('likes 5')
 })
