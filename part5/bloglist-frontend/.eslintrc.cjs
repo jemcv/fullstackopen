5c7bacd51ea444dc096670c3574143b0086b846e
jemcv <rangothechameleon@gmail.com>
2024-09-07 18:14:54 +0800

    Done part5 bloglist-e2e-testing step1 5.17

--- part5/bloglist-frontend/.eslintrc.cjs
+++ part5/bloglist-frontend/.eslintrc.cjs
@@ -4,6 +4,8 @@ module.exports = {
     browser: true,
     es2020: true,
     'vitest-globals/env': true,
+    'jest/globals': true,
+    "cypress/globals": true
   },
   extends: [
     'eslint:recommended',
@@ -15,7 +17,7 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', "react", "jest", "cypress", "vite"],
   rules: {
     "indent": [
         "error",
