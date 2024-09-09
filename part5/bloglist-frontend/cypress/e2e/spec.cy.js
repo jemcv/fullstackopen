8f1d8ef37f057aaac1c62700545253152fd2b213
jemcv <rangothechameleon@gmail.com>
2024-09-09 15:28:10 +0800

    Done part5 bloglist-e2e-testing step2 5.18

--- part5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -6,4 +6,16 @@ describe('Blog app', function() {
   it('Login form is shown', function() {
     cy.contains('Log in to application')
   })
+  it('Successful login', function() {
+    cy.get('#username').type('test')
+    cy.get('#password').type('test')
+    cy.get('#login-button').click()
+  })   
+  it('Unsuccessful login', function() {
+    cy.get('#username').type('test')
+    cy.get('#password').type('wrong')
+    cy.get('#login-button').click()
+    cy.get('.error').should('contain', 'Wrong credentials')
+    cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
+  })
 })
