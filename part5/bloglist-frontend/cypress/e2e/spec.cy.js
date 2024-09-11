5e0bf9378c37ac6c201d9377e4ef74ed6ce6ec1f
jemcv <rangothechameleon@gmail.com>
2024-09-11 19:35:32 +0800

    Done part5 bloglist-e2e-testing step7 5.23

--- part5/bloglist-frontend/cypress/e2e/spec.cy.js
+++ part5/bloglist-frontend/cypress/e2e/spec.cy.js
@@ -32,10 +32,13 @@ describe('Blog app', function() {
 
 describe('When logged in', function() {
   beforeEach(function() {
-    cy.visit('http://localhost:5173')
-    cy.get('#username').type('123123')
-    cy.get('#password').type('123123')
-    cy.get('#login-button').click()
+    cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
+      username: '123123',
+      password: '123123'
+    }).then(({ body }) => {
+      localStorage.setItem('loggedBlogAppUser', JSON.stringify(body))
+      cy.visit('http://localhost:5173')
+    })
   })
 
   it('A blog can be created', function() {
@@ -57,10 +60,13 @@ describe('Only the creator can see the delete button', function() {
       password: '123123'
     }
     cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
-    cy.visit('http://localhost:5173')
-    cy.get('#username').type('123123')
-    cy.get('#password').type('123123')
-    cy.get('#login-button').click()
+    cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
+      username: '123123',
+      password: '123123'
+    }).then(({ body }) => {
+      localStorage.setItem('loggedBlogAppUser', JSON.stringify(body))
+      cy.visit('http://localhost:5173')
+    })
     cy.contains('create new').click()
     cy.get('#title').type('a blog created by cypress')
     cy.get('#author').type('cypress')
@@ -77,11 +83,32 @@ describe('Only the creator can see the delete button', function() {
       password: '12341234'
     }
     cy.request('POST', `${Cypress.env('BACKEND')}/users`, anotherUser)
-    cy.visit('http://localhost:5173')
-    cy.get('#username').type('12341234')
-    cy.get('#password').type('12341234')
-    cy.get('#login-button').click()
+    cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
+      username: '12341234',
+      password: '12341234'
+    }).then(({ body }) => {
+      localStorage.setItem('loggedBlogAppUser', JSON.stringify(body))
+      cy.visit('http://localhost:5173')
+    })
     cy.contains('view').click()
     cy.contains('remove').should('not.exist')
+  })
+
+  it('Blogs are ordered by likes', function() {
+    cy.request('POST', `${Cypress.env('BACKEND')}/login`, {
+      username: '123123',
+      password: '123123'
+    }).then(({ body }) => {
+      localStorage.setItem('loggedBlogAppUser', JSON.stringify(body))
+      cy.visit('http://localhost:5173')
+    })
+
+    cy.createBlog({ title: 'first blog', author: 'cypress', url: 'url1.com', likes: 1 })
+    cy.createBlog({ title: 'second blog', author: 'cypress', url: 'url2.com', likes: 2 })
+    cy.createBlog({ title: 'third blog', author: 'cypress', url: 'url3.com', likes: 3 })
+
+    cy.get('.blog').eq(0).should('contain', 'third blog')
+    cy.get('.blog').eq(1).should('contain', 'second blog')
+    cy.get('.blog').eq(2).should('contain', 'first blog')
   })
 })
