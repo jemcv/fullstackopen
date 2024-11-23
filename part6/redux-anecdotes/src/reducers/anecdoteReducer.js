e694f71f6ef3ec0f0dce21cf803b263659e99c41
jemcv <rangothechameleon@gmail.com>
2024-11-23 08:02:19 +0800

    Done part1 redux-anecdotes step1

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,29 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_VOTE':
+      return state.map(anecdote =>
+        anecdote.id === action.payload.id
+          ? { ...anecdote, votes: anecdote.votes + 1 }
+          : anecdote
+      )
+
+    default:
+      break;
+  }
+
   console.log('state now: ', state)
   console.log('action', action)
 
   return state
+}
+
+export const addVote = (id) => {
+  return {
+    type: 'ADD_VOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
